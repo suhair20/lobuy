a8539e754e537bd17b8af6cd7ad3f08dc73b81dd
suhair20 <moidheensuhair@gmail.com>
2026-02-23 16:46:27 +0530

    pushh-all

--- backend/middleware/UserCheckAuth.js
+++ backend/middleware/UserCheckAuth.js
@@ -1,28 +1,23 @@
 
 import jwt from "../Services/jwt.js";
-
 export function UserCheckAuth(req, res, next) {
+  console.time("TOKEN_VERIFY"); // <-- START TIMER
 
-  
-
-  
   const token = req.cookies.token;
 
- 
-  
   if (!token) {
+    console.timeEnd("TOKEN_VERIFY");
     return res.status(401).json({ message: "Not logged in" });
   }
 
   try {
-    const user = jwt.verifyToken(token)
+    const user = jwt.verifyToken(token);
     req.user = user;
-   
     
-
-
+    console.timeEnd("TOKEN_VERIFY"); // <-- END TIMER
     next();
   } catch (err) {
+    console.timeEnd("TOKEN_VERIFY");
     return res.status(401).json({ message: "Invalid token" });
   }
 }
